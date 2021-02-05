2640f5a4961bba89db74646a814cce900a023b70
Khairo Khatib <khairo.kh@outlook.com>
2021-02-05 02:54:57 -0500

    Added frontend signup and login pages

--- instagram/frontend/src/components/HomePage.js
+++ instagram/frontend/src/components/HomePage.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
+import Signup from "./SignUp";
+import Login from "./Login";
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link,
+Redirect
+} from 'react-router-dom';
 
 const HomePage = () => {
-   
-      return(
-        <div>
-          <h1>Setup is working correctly!!</h1>
-        </div>
-      );
-  };
+  // Adding react Router in order to align the URL with the intended page/component
+  // Make sure to start from most specific to least specific paths! 
+  return(
+    <Router>
+      <Switch> 
+        <Route path='/signup' component={Signup}></Route>
+        <Route path='/login' component={Login}></Route>
+        <Route exact path='/'><p>Home page to be implemented!!!!</p></Route>
+      </Switch>
+    </Router> 
+  );    
+};
 
   export default HomePage;
