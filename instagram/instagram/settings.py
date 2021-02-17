0e5e7a74419dfd5566c5591a91b8c8936527ff64
Massimo <mlopez9876@gmail.com>
2021-02-17 14:47:11 -0500

    Created a database for posts #15 Created a posting feature using django (no rest api). It is still a work in progress. - it can add post pictures based on user. #15 - whoever posts a photo can select which user to post as which is a problem. - made changes to pipfile to add new packages need so do a 'pipenv install' in terminal before running the server.

--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ INSTALLED_APPS = [
     'frontend.apps.FrontendConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
-
+    'posts.apps.PostsConfig'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
