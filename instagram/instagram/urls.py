9f1b231eaa10637c8615d2b3a1c549c601b09281
Massimo <mlopez9876@gmail.com>
2021-02-17 14:47:11 -0500

    Created a posting feature Created a posting feature using django (no rest api). It is still a work in progress. - it can add post pictures based on user. - whoever posts a photo can select which user to post as which is a   problem. - made changes to pipfile to add new packages need so do a 'pipenv   install' in terminal before running the server.

--- instagram/instagram/urls.py
+++ instagram/instagram/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from . import settings # For Having access to the media root
+from django.conf.urls.static import static 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
-
+from posts import views as post_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register,name ='register'),
@@ -25,6 +28,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('newpost/', post_views.new_post, name = 'newpost'),
     path('profile/', user_views.profile, name='profile'),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
