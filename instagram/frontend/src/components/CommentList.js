164b8010848ecab04ec1613c034e7a887718f5f2
Khairo Khatib <khairo.kh@outlook.com>
2021-03-14 15:59:21 -0400

    #22 fixed bugs related to React API context for displaying comments and removed unnecessary code

--- instagram/frontend/src/components/CommentList.js
+++ instagram/frontend/src/components/CommentList.js
@@ -12,44 +12,34 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken';
  
 
 const CommentList = () => {
-  const {comments , setComments} = useContext(CommentsContext);
+  const {comment , setComment} = useContext(CommentsContext);
   const {LoggedInUserInfo, setLoggedInUserInfo} = useContext(UserContext);
   const {isLoggedIn, setIsLoggedIn} = useContext(UserStatusContext);
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const localPosts = localStorage.getItem("userInfo");
 
-  const getProfileImage = axios.get('https://faceapi.herokuapp.com/faces?n=1')
-    .then((res) => {
-      return res.data;
-    }).catch(err => {
-      console.error(err);
-    })
-  
- 
-console.log(getProfileImage);
-  
-  
   
   useEffect( () => {
     axios.get('comments/')
       .then((response) => {
-        setComments(response.data);
+        setComment(response.data);
         // console.log(response.data)
       }).catch(err => {
         console.error(err);
       })
-    setLoading(false);
+    
   },[])
 
   useEffect( () => {
-    localStorage.setItem("comments", JSON.stringify(comments));
-  },[comments])
+    localStorage.setItem("comments", JSON.stringify(comment));
+    setIsLoading(false);
+  },[comment])
 
 
 
     return(
       <>
-        {loading? <div>Loading ...</div> : comments.map((item) => (
+        {isLoading? <div>Loading ...</div> : comment.map((item) => (
           <Comments username={item.owner}  avatar={ LoggedInUserInfo.avatar || JSON.parse(localPosts).avatar }  
           comment={item.body} key={item.id} />
         ))}
