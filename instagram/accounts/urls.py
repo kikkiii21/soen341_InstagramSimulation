10cd50bc56bf83351e8552ea63f7615876167df9
kikkiii21 <43822671+kikkiii21@users.noreply.github.com>
2021-03-15 15:06:44 -0400

    Update urls.py
    Added the logout path, for some reason it had disapeared

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
 	path('users/', UserListAPI.as_view()),
 	path('user/<int:pk>/', UserAPI.as_view()),
 	path('follow/', FollowAPI.as_view()),
-	path('followingEndpoint/', FollowedPostsAPI.as_view())
+	path('followingEndpoint/', FollowedPostsAPI.as_view()),
+	path('logout', knox_views.LogoutView.as_view(), name = 'knox_logout'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
