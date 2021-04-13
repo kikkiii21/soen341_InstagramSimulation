892da7880233bf86e1e620445dc936ec2d2a8503
Massimo Lopez <mlopez9876@gmail.com>
2021-04-13 02:17:01 -0400

    issue #67 doing posting picture test, not complete yet.

--- instagram/posts/tests/test_views.py
+++ instagram/posts/tests/test_views.py
@@ -1,60 +1,65 @@
-# import json
-# from rest_framework import status
-# from django.test import TestCase, Client
-# from django.urls import reverse
-# from ..models import Post
-# from ..serializers import PostSerializer
-# # from ...accounts.tests.test_views import GetAllUsersTest
-# from django.contrib.auth.models import User
-# # from django.contrib.auth import Image
-# from django.core.files.uploadedfile import SimpleUploadedFile
-# import io    
-# # from django.utils.image import Image
-# import datetime
+import json
 
-# # Initialize an Image
-# # fp = io.BytesIO()
-# # Image.new('test', (1,1)).save(fp, 'png')
-# # fp.seek(0)
-# # portrait = SimpleUploadedFile(name=''foo, content=fp.read())
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from PIL import Image
+import tempfile
 
-# # initialize the APIClient app
-# client = Client()
+from ..models import Post
+from ..serializers import PostSerializer
 
 
-# class TestPostViews(TestCase):
-#     def setUp(self):
-#         self.user1 = User.objects.create(
-#             first_name='Casper', 
-#             last_name='Patel', 
-#             username='BullDog', 
-#             email='casper@gmail.com', 
-#             password='123'
-#         )
-#         self.client = Client()
-#         self.post_list_url = reverse('post_list')
-#         # self.post_new_picture_url = reverse('post')
+# print(Image.open('baby_kilo.png').format)
 
-#     def test_get_all_posts_GET(self):
-#         response = self.client.get(self.post_list_url)
-#         posts = Post.objects.all()
-#         serializer = PostSerializer(posts, many=True)
-#         self.assertEqual(response.data, serializer.data)
-#         self.assertEquals(response.status_code, status.HTTP_200_OK)
+class PostingPictureTestCase(APITestCase):
 
-#     def test_post_picture_POST(self):
-#         response = self.client.post(self.post_list_url, {
-#             'caption': 'This is a caption',
-#             'photo': SimpleUploadedFile(
-#                 name='foo.gif', 
-#                 content=b'GIF87a\x01\x00\x01\x00\x80\x01\x00\x00\x00\x00ccc,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00'
-#             ),
-#             # 'photo': SimpleUploadedFile(name='test_image.jpg', content=open(image_path, 'rb').read(), content_type='image/jpeg')
-#             # 'author': self.user1,
-#             # 'created_at': datetime.date.today()
-#         })
-#         posts = Post.objects.all()
-#         # serializer = PostSerializer(posts, many = True)
-#         # self.assertEqual(response.data, serializer.data)
-#         self.assertEquals(response.status_code, 302)
-#         self.assertEquals(Post.objects.first().created_at, datetime.date.today())
+    def setUp(self):
+        self.user_jason = User.objects.create(username='jason', email='jason@gmail.com', password='123')
+        # self.user_massimo = User.objects.create(username='massimo', email='massimo@gmail.com', password='123')
+        # Post.objects.create(caption='Dis cat', photo='profile_pics/Screen_Shot_2021-01-22_at_1.42.14_P.png', author=user_jason)
+        # Post.objects.create(caption='I like cat', photo='profile_pics/Screen_Shot_2021-01-22_at_1.42.14_P.png', author=user_massimo)
+        
+        # self.token_massimo = Token.objects.create(user=self.user_massimo)
+        # self.token_jason = Token.objects.create(user=self.user_jason)
+        img = Image.open('instagram/images/baby_kilo.png')
+        print(img)
+        self.data = {
+            "caption": "Dis cat",
+            "photo": img
+        }
+        img.close()
+        
+
+    def test_posting_picture(self):
+        # Log user in
+        self.client.login(username=self.user_jason.username, password='123')
+
+        
+        response = self.client.post('postsEndpoint/', self.data, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # self.client.credentials(HTTP_AUTHORIZATION='Token' + self.token_massimo)
+        img.close()
+
+
+    def test_post_photo(self):
+        """
+        Test trying to add a photo
+        """
+        # Create an album
+        album = AlbumFactory(owner=self.user)
+
+        # Log user in
+        self.client.login(username=self.user.username, password='password')
+
+        # Create image
+        image = Image.new('RGB', (100, 100))
+        tmp_file = tempfile.NamedTemporaryFile(suffix='.jpg')
+        image.save(tmp_file)
+
+        # Send data
+        with open(tmp_file.name, 'rb') as data:
+            response = self.client.post(reverse('photo-list'), {'album': 'http://testserver/api/albums/' + album.pk, 'image': data}, format='multipart')
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
