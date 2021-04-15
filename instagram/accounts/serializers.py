084ffefbb785b494d1c9f98adaaaf4738bdd4102
Massimo Lopez <mlopez9876@gmail.com>
2021-04-15 19:47:04 -0400

    issue #73, removed extra space in line 93

--- instagram/accounts/serializers.py
+++ instagram/accounts/serializers.py
@@ -89,7 +89,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], first_name=validated_data['first_name'],
-         last_name=validated_data['last_name'], email=validated_data['email'], password=validated_data['password'])
+        last_name=validated_data['last_name'], email=validated_data['email'], password=validated_data['password'])
         return user
 
 
